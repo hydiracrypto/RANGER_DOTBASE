986c041c19581639648944cba7ac4a3f0dca2a7a
blocxerz <98099317+hydiracrypto@users.noreply.github.com>
2026-06-14 12:31:46 +0700

    Add files via upload

---
+++ leaderboard.js
@@ -0,0 +1,56 @@
+window.scoreSubmitted = false;
+
+async function saveLeaderboardScore(score){
+
+  try{
+
+    await supabaseClient
+      .from("leaderboard")
+      .insert({
+        wallet: currentWallet,
+        username: currentUsername || "",
+        fid: currentFID || null,
+        score: score
+      });
+
+  }catch(err){
+    console.error(err);
+  }
+
+}
+
+async function showLeaderboard(){
+console.log("Leaderboard Clicked");
+console.log(data);
+
+const { data,error } =
+await supabaseClient
+.from("leaderboard")
+.select("*")
+.order("score",{ascending:false})
+.limit(10);
+
+if(error){
+console.error(error);
+return;
+}
+
+document.getElementById(
+"leaderboard-panel"
+).style.display="block";
+
+document.getElementById(
+"leaderboard-list"
+).innerHTML =
+(data||[])
+.map((row,index)=>
+`<div style="padding:6px;border-bottom:1px solid #333">
+#${index+1}
+${row.username || row.wallet || "Player"}
+
+- ${row.score}
+
+ </div>`
+ )
+ .join("");
+}
